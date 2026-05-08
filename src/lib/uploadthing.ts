83a884008809d990da354279155361eed3be6e31
Runoxdd <runoefekemo@gmail.com>
2026-05-08 17:05:51 +0100

    Updated admin pages and upload logic

--- src/lib/uploadthing.ts
+++ src/lib/uploadthing.ts
@@ -3,7 +3,7 @@ import {
   generateUploadDropzone,
 } from "@uploadthing/react";
 
-import type { UploadRouter } from "@/app/api/upload/core";
+import type { UploadRouter } from "@/app/api/uploadthing/core";
 
 export const UploadButton = generateUploadButton<UploadRouter>();
 export const UploadDropzone = generateUploadDropzone<UploadRouter>();
